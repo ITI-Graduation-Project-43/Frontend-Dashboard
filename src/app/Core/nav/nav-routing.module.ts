aed92b32f4a7a74d2cf25dc59a7dd92464a16766
Abdelrahman Elsayed <abd-elrahman_elsayed@outlook.com>
2023-06-26 02:37:53 +0300

    init power bi report

--- src/app/Core/nav/nav-routing.module.ts
+++ src/app/Core/nav/nav-routing.module.ts
@@ -17,6 +17,11 @@ const routes: Routes = [
     path: 'instructor',
     loadChildren: () => import('../../Components/instructor/instructor.module')
       .then((m) => m.InstructorModule)
+  },
+  {
+    path: 'bi',
+    loadChildren: () => import('../../Components/power-bi-report/power-bi-report.module')
+      .then((m) => m.PowerBiReportModule)
   }
 ];
 
