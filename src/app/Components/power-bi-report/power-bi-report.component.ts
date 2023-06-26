091ad8b0a54c0f452ac82a39d0f5f4d3273fe0c0
Abdelrahman Elsayed <abd-elrahman_elsayed@outlook.com>
2023-06-26 06:51:16 +0300

    power bi first view

--- src/app/Components/power-bi-report/power-bi-report.component.ts
+++ src/app/Components/power-bi-report/power-bi-report.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-power-bi-report',
   templateUrl: './power-bi-report.component.html',
   styleUrls: ['./power-bi-report.component.scss']
 })
-export class PowerBiReportComponent {
 
+
+
+export class PowerBiReportComponent {
+  
 }
