9367e74f97fa5a447c049697c0f11c7cad049d79
Mahmoud Mohamed Ali ALi <99610976+iMahmouddAli@users.noreply.github.com>
2023-06-27 07:57:29 +0300

    add pagination to Courses

--- src/app/Components/Courses/courses.component.ts
+++ src/app/Components/Courses/courses.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Course } from 'src/app/Models/Course';
-import { CourseService } from '../../services/Course.service';
 import { APIResponseVM } from 'src/app/shared/ViewModels/apiresponse-vm';
 import { CourseOverviewComponent } from './Components/course-overview/course-overview.component';
 import { NotificationService } from 'src/app/shared/Services/notification.service';
 import { Router } from '@angular/router';
+import { CourseService } from 'src/app/services/Course.service';
+import { PageEvent } from '@angular/material/paginator';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
@@ -28,9 +29,9 @@ export class CoursesComponent implements OnInit {
     if (id == 0) return;
     else {
       let observer = {
-        next: () => {
-          console.log('Approve');
+        next: (data: any) => {
           this.notification.notify('Course has been approved', 'success');
+          this.fetchData();
         },
         error: () => console.log('error'),
       };
@@ -52,11 +53,21 @@ export class CoursesComponent implements OnInit {
     });
   }
 
-  fetchData() {
+  fetchData(pageNumber: number = 1, pageSize: number = 5) {
     this.apiService
-      .getAllItem('Course/nonApprovedCourses')
+      .getAllItem(
+        `Course/nonApprovedCourses?pageNumber=${pageNumber}&pageSize=${pageSize}`
+      )
       .subscribe((data: APIResponseVM) => {
         this.courses = data.items;
+        this.loading = false;
       });
+  }
+
+  onPageChange(event: PageEvent) {
+    this.loading = true;
+    const startIndex = event.pageIndex * event.pageSize;
+    const endIndex = startIndex + event.pageSize;
+    this.fetchData(startIndex, endIndex);
   }
 }
